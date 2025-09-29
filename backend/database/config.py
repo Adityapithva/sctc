7b0c8ec2b11269010fa2255e6f5dfa1fcfc1f52b
bhargav bhatt <bhargavmbhatt24@gmail.com>
2025-09-29 23:57:57 +0530

    react build

--- backend/database/config.py
+++ backend/database/config.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://theboys8122422_db_user:T3I97VzscegvDahL@sctc.wy7mzow.mongodb.net/?retryWrites=true&w=majority&appName=sctc")
 
 db = client["smart_classroom"]
 
